02182ed574abce409f5c5e29e4bc4b5f65fc6ac4
nisha0208 <65272574+nisha0208@users.noreply.github.com>
2024-12-20 16:07:37 +0530

    Add files via upload

---
+++ MessagingQueueAPI/MessagingQueueAPI/Controllers/OrdersController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+
+namespace MessagingQueueAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public OrdersController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateOrderAsync([FromBody] Order order)
+        {
+            IQueueClient queueClient = new QueueClient(_configuration["QueueConnectionString"], _configuration["QueueName"]);
+            var orderJSON = JsonConvert.SerializeObject(order);
+            var orderMessage = new Message(Encoding.UTF8.GetBytes(orderJSON))
+            {
+                MessageId = Guid.NewGuid().ToString(),
+                ContentType = "application/json"
+            };
+
+            await queueClient.SendAsync(orderMessage).ConfigureAwait(false);
+
+            return Ok("Create order message has been successfully pushed to queue");
+        }
+    }
+}
